bb7a96d6161d9f94571c2411391732625d5c546f
Simon Barnett <si@medatechuk.com>
2017-07-10 14:27:43 +0100

    Initial Commit

---
+++ iDocument.vb
@@ -0,0 +1,21 @@
+﻿Public Class iDocument : Inherits List(Of System.Drawing.Image)
+
+    Private _barcode As String
+
+    Public ReadOnly Property scan_type_code() As String
+        Get
+            Return _barcode.Substring(0, 5).Replace(" ", "")
+        End Get
+    End Property
+
+    Public ReadOnly Property scan_doc_code() As String
+        Get
+            Return _barcode.Substring(6).Replace(" ", "")
+        End Get
+    End Property
+
+    Public Sub New(ByVal Barcode As String)
+        _barcode = Barcode
+    End Sub
+
+End Class
